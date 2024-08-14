6933d38175847bff384115bcb07ffa4e9d94392c
Himanshu8876 <garghimanshu778@gmail.com>
2024-08-14 23:27:16 +0530

    final commit!

--- src/apis/configs/axiosConfigs.ts
+++ src/apis/configs/axiosConfigs.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from "axios";
 
 export const api = axios.create({
   withCredentials: true,
-  baseURL:"http://13.127.109.183:8080",
+  baseURL:"http://localhost:8080",
   
 });
 
