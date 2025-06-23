6cc3a75d21e55cf41fe106ce3f0a0b9f896333d5
Yashaswi Poddar <46918800+whateverhappenshappens@users.noreply.github.com>
2025-06-23 17:19:16 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
